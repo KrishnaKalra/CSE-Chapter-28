0259332ed1ca1996b68cc7c77f3bab6b1ec10b40
amanraj <raj.aman4001@gmail.com>
2025-10-08 16:04:25 +0530

    fix: axios get callback to server

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -17,7 +17,7 @@ function header() {
     console.log(year);
     let id = user.email.slice(0, 7).toUpperCase();
     const check = await axios.get(
-      "http://localhost:300/api/" + year + "/id?id=" + id
+      "https://cse-chapter-28-server.vercel.app/api/" + year + "/id?id=" + id
     );
     if (check.data.length == 0) {
       try {
